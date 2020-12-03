1deeb75bbaccf0564994f3c3f3fe512bae4549ba
manonlt <61652364+manonlt@users.noreply.github.com>
2020-12-03 17:12:42 +0100

    Create Analyse de fréquences.py

---
+++ Analyse de fréquences.py
@@ -0,0 +1,19 @@
+# Consigne: On souhaite déterminer le nombre de mots de 1 lettre, 2 lettres, 3 lettres… que contient un texte.
+# Le texte contient un ensemble de mots, séparés par des espaces, sans aucun signe de ponctuation
+
+# Exemple texte : 
+# La cigale ayant chanté
+# Tout l'été,
+# Se trouva fort dépourvue
+# Quand la bise fut venue.
+
+phrase = input() 
+
+liste = []
+for mot in phrase.split():
+  liste.append(len(mot))
+
+for i in range(max(liste) + 1):
+  if liste.count(i) != 0:
+    print(i, ':', liste.count(i))
+        
