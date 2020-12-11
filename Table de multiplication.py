eb259fe2737bcd082a3f1be71444915cb8040b27
manonlt <61652364+manonlt@users.noreply.github.com>
2020-12-11 16:50:33 +0100

    Create Table de multiplication.py

---
+++ Table de multiplication.py
@@ -0,0 +1,7 @@
+# Écrivez un programme qui affiche une table de multiplication allant jusqu'à 20 fois 20.
+
+
+for i in range(1,21):
+    print()
+    for j in range(1,21):
+        print(i*j, end=' ')
